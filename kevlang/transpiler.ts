06240e696f872e49dd76fb8ea93d7b22aef95569
binarykevz <107824678+binarykevz@users.noreply.github.com>
2026-08-11 21:52:27 +0800

    Update transpiler.ts

--- kevlang/transpiler.ts
+++ kevlang/transpiler.ts
@@ -323,6 +323,11 @@ export function transpile(source: string, filename: string = "unknown.kev", isIn
             i++;
             continue;
         }
+if (t.type === "REACTION") {
+            out.push("=>");
+            i++;
+            continue;
+        }
 
         // --- Pass-through ---
         out.push(emitToken(t) + (needsSpace(t, tokens[i + 1]) ? " " : ""));
